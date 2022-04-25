90896ea4d657d07071ff580d41aa0f9516a08f5e
SHIM <weasd33@naver.com>
2022-04-25 22:00:08 +0900

    JNDI

--- web05/src/main/java/spms/dao/MemberDao.java
+++ web05/src/main/java/spms/dao/MemberDao.java
@@ -1,5 +1,6 @@
 package spms.dao;
 
+// DataSource 활용 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,160 +8,174 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import spms.util.DBConnectionPool;
+import javax.sql.DataSource;
+
 import spms.vo.Member;
 
 public class MemberDao {
-	DBConnectionPool connPool;
-	
-	public void setDbConnectionPool(DBConnectionPool connPool) {
-		this.connPool = connPool;
-	}
+  DataSource ds;
 
-	public List<Member> selectList() throws Exception {
-		Connection connection = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+  public void setDataSource(DataSource ds) {
+    this.ds = ds;
+  }
 
-		try {
-			connection = connPool.getConnection();
-			stmt = connection.createStatement();
-			rs = stmt.executeQuery("SELECT MNO,MNAME,EMAIL,CRE_DATE FROM MEMBERS ORDER BY MNO ASC");
+  public List<Member> selectList() throws Exception {
+    Connection connection = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    
+    try {
+      connection = ds.getConnection();
+      stmt = connection.createStatement();
+      rs = stmt.executeQuery(
+          "SELECT MNO,MNAME,EMAIL,CRE_DATE" + 
+              " FROM MEMBERS" +
+          " ORDER BY MNO ASC");
 
-			ArrayList<Member> members = new ArrayList<Member>();
+      ArrayList<Member> members = new ArrayList<Member>();
 
-			while (rs.next()) {
-				members.add(new Member()
-						.setNo(rs.getInt("MNO"))
-						.setName(rs.getString("MNAME"))
-						.setEmail(rs.getString("EMAIL"))
-						.setCreatedDate(rs.getDate("CRE_DATE")));
-			}
+      while(rs.next()) {
+        members.add(new Member()
+        .setNo(rs.getInt("MNO"))
+        .setName(rs.getString("MNAME"))
+        .setEmail(rs.getString("EMAIL"))
+        .setCreatedDate(rs.getDate("CRE_DATE"))	);
+      }
 
-			return members;
+      return members;
 
-		} catch (Exception e) { throw e; } 
-		finally {
-			try { if (rs != null) rs.close(); } catch (Exception e) {}
-			try { if (stmt != null) stmt.close(); } catch (Exception e) {}
-			if (connection != null) connPool.returnConnection(connection);
-		}
-	}
+    } catch (Exception e) {
+      throw e;
 
-	public int insert(Member member) throws Exception {
-		Connection connection = null;
-		PreparedStatement stmt = null;
+    } finally {
+      try {if (rs != null) rs.close();} catch(Exception e) {}
+      try {if (stmt != null) stmt.close();} catch(Exception e) {}
+      try {if (connection != null) connection.close();} catch(Exception e) {}
+    }
+  }
 
-		try {
-			connection = connPool.getConnection();
-			stmt = connection.prepareStatement(
-					"INSERT INTO MEMBERS(EMAIL,PWD,MNAME,CRE_DATE,MOD_DATE) VALUES (?, ?, ?, SYSDATE, SYSDATE)");
-			stmt.setString(1, member.getEmail());
-			stmt.setString(2, member.getPassword());
-			stmt.setString(3, member.getName());
-			return stmt.executeUpdate();
+  public int insert(Member member) throws Exception  {
+    Connection connection = null;
+    PreparedStatement stmt = null;
 
-		} catch (Exception e) {
-			throw e;
+    try {
+      connection = ds.getConnection();
+      stmt = connection.prepareStatement(
+          "INSERT INTO MEMBERS(EMAIL,PWD,MNAME,CRE_DATE,MOD_DATE)"
+              + " VALUES (?,?,?,NOW(),NOW())");
+      stmt.setString(1, member.getEmail());
+      stmt.setString(2, member.getPassword());
+      stmt.setString(3, member.getName());
+      return stmt.executeUpdate();
 
-		} finally {
-			try { if (stmt != null) stmt.close(); } catch (Exception e) {}
-			if (connection != null) connPool.returnConnection(connection);
-		}
-	}
+    } catch (Exception e) {
+      throw e;
 
-	public int delete(int no) throws Exception {
-		Connection connection = null;
-		Statement stmt = null;
+    } finally {
+      try {if (stmt != null) stmt.close();} catch(Exception e) {}
+      try {if (connection != null) connection.close();} catch(Exception e) {}
+    }
+  }
 
-		try {
-			connection = connPool.getConnection();
-			stmt = connection.createStatement();
-			return stmt.executeUpdate("DELETE FROM MEMBERS WHERE MNO=" + no);
+  public int delete(int no) throws Exception {  
+    Connection connection = null;
+    Statement stmt = null;
 
-		} catch (Exception e) {
-			throw e;
+    try {
+      connection = ds.getConnection();
+      stmt = connection.createStatement();
+      return stmt.executeUpdate(
+          "DELETE FROM MEMBERS WHERE MNO=" + no);
 
-		} finally {
-			try { if (stmt != null) stmt.close(); } catch (Exception e) {}
-			if (connection != null) connPool.returnConnection(connection);
-		}
-	}
+    } catch (Exception e) {
+      throw e;
 
-	public Member selectOne(int no) throws Exception {
-		Connection connection = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try {
-			connection = connPool.getConnection();
-			stmt = connection.createStatement();
-			rs = stmt.executeQuery("SELECT MNO,EMAIL,MNAME,CRE_DATE FROM MEMBERS WHERE MNO=" + no);
-			if (rs.next()) {
-				return new Member()
-						.setNo(rs.getInt("MNO"))
-						.setEmail(rs.getString("EMAIL"))
-						.setName(rs.getString("MNAME"))
-						.setCreatedDate(rs.getDate("CRE_DATE"));
+    } finally {
+      try {if (stmt != null) stmt.close();} catch(Exception e) {}
+      try {if (connection != null) connection.close();} catch(Exception e) {}
+    }
+  }
 
-			} else {
-				throw new Exception("해당 번호의 회원을 찾을 수 없습니다.");
-			}
+  public Member selectOne(int no) throws Exception { 
+    Connection connection = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    try {
+      connection = ds.getConnection();
+      stmt = connection.createStatement();
+      rs = stmt.executeQuery(
+          "SELECT MNO,EMAIL,MNAME,CRE_DATE FROM MEMBERS" + 
+              " WHERE MNO=" + no);    
+      if (rs.next()) {
+        return new Member()
+        .setNo(rs.getInt("MNO"))
+        .setEmail(rs.getString("EMAIL"))
+        .setName(rs.getString("MNAME"))
+        .setCreatedDate(rs.getDate("CRE_DATE"));
 
-		} catch (Exception e) {
-			throw e;
-		} finally {
-			try { if (rs != null) rs.close(); } catch (Exception e) {}
-			try { if (stmt != null) stmt.close(); } catch (Exception e) {}
-			if (connection != null) connPool.returnConnection(connection);
-		}
-	}
+      } else {
+        throw new Exception("해당 번호의 회원을 찾을 수 없습니다.");
+      }
 
-	public int update(Member member) throws Exception {
-		Connection connection = null;
-		PreparedStatement stmt = null;
-		
-		try {
-			connection = connPool.getConnection();
-			stmt = connection.prepareStatement("UPDATE MEMBERS SET EMAIL=?,MNAME=?,MOD_DATE=SYSDATE WHERE MNO=?");
-			stmt.setString(1, member.getEmail());
-			stmt.setString(2, member.getName());
-			stmt.setInt(3, member.getNo());
-			return stmt.executeUpdate();
+    } catch (Exception e) {
+      throw e;
+    } finally {
+      try {if (rs != null) rs.close();} catch(Exception e) {}
+      try {if (stmt != null) stmt.close();} catch(Exception e) {}
+      try {if (connection != null) connection.close();} catch(Exception e) {}
+    }
+  }
 
-		} catch (Exception e) {
-			throw e;
+  public int update(Member member) throws Exception { 
+    Connection connection = null;
+    PreparedStatement stmt = null;
+    try {
+      connection = ds.getConnection();
+      stmt = connection.prepareStatement(
+          "UPDATE MEMBERS SET EMAIL=?,MNAME=?,MOD_DATE=now()"
+              + " WHERE MNO=?");
+      stmt.setString(1, member.getEmail());
+      stmt.setString(2, member.getName());
+      stmt.setInt(3, member.getNo());
+      return stmt.executeUpdate();
 
-		} finally {
-			try { if (stmt != null) stmt.close(); } catch (Exception e) {}
-			if (connection != null) connPool.returnConnection(connection);
-		}
-	}
+    } catch (Exception e) {
+      throw e;
 
-	public Member exist(String email, String password) throws Exception {
-		Connection connection = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
+    } finally {
+      try {if (stmt != null) stmt.close();} catch(Exception e) {}
+      try {if (connection != null) connection.close();} catch(Exception e) {}
+    }
+  }
+  
+  public Member exist(String email, String password) throws Exception {
+    Connection connection = null;
+    PreparedStatement stmt = null;
+    ResultSet rs = null;
 
-		try {
-			connection = connPool.getConnection();
-			stmt = connection.prepareStatement("SELECT MNAME,EMAIL FROM MEMBERS WHERE EMAIL=? AND PWD=?");
-			stmt.setString(1, email);
-			stmt.setString(2, password);
-			rs = stmt.executeQuery();
-			if (rs.next()) {
-				return new Member().setName(rs.getString("MNAME")).setEmail(rs.getString("EMAIL"));
-			} else {
-				return null;
-			}
-		} catch (Exception e) {
-			throw e;
+    try {
+      connection = ds.getConnection();
+      stmt = connection.prepareStatement(
+          "SELECT MNAME,EMAIL FROM MEMBERS"
+              + " WHERE EMAIL=? AND PWD=?");
+      stmt.setString(1, email);
+      stmt.setString(2, password);
+      rs = stmt.executeQuery();
+      if (rs.next()) {
+        return new Member()
+          .setName(rs.getString("MNAME"))
+          .setEmail(rs.getString("EMAIL"));
+      } else {
+        return null;
+      }
+    } catch (Exception e) {
+      throw e;
 
-		} finally {
-			try { if (rs != null) rs.close(); } catch (Exception e) {}
-			try { if (stmt != null) stmt.close(); } catch (Exception e) {}
-			if (connection != null) connPool.returnConnection(connection);
-		}
-	}
+    } finally {
+      try {if (rs != null) rs.close();} catch (Exception e) {}
+      try {if (stmt != null) stmt.close();} catch (Exception e) {}
+      try {if (connection != null) connection.close();} catch(Exception e) {}
+    }
+  }
 
 }
--- web05/src/main/java/spms/listeners/ContextLoaderListener.java
+++ web05/src/main/java/spms/listeners/ContextLoaderListener.java
@@ -1,32 +1,31 @@
 package spms.listeners;
 
+import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
 
 import spms.dao.MemberDao;
-import spms.util.DBConnectionPool;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
-	DBConnectionPool connPool;
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		// contextInitialized() - 어플리케이션이 실행될 때 호출
 		try {
 			ServletContext sc = event.getServletContext();
-
-			connPool = new DBConnectionPool(
-					sc.getInitParameter("driver"),
-					sc.getInitParameter("url"),
-					sc.getInitParameter("username"),
-					sc.getInitParameter("password"));
-
+		      
+			InitialContext initialContext = new InitialContext();
+			// lookup 메서드의 매개변수 값은 서버 자원의 JNDI 이름 앞에 "java:comp/env"를 붙임
+			// 반환형은 Object로 형변환을 해줌
+			DataSource ds = (DataSource)initialContext.lookup("java:comp/env/jdbc/oracle");
+	      
 			MemberDao memberDao = new MemberDao();
-			memberDao.setDbConnectionPool(connPool);
-
+			memberDao.setDataSource(ds);
+	      
 			// DB 연동한 memberDAO를 다른 서블릿에서도 사용하기 위해 set
 			// 다른 서블릿에서는 memberDAO를 get해서 재사용해주면 됨
 			sc.setAttribute("memberDao", memberDao);
@@ -39,6 +38,5 @@ public class ContextLoaderListener implements ServletContextListener {
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
 		// contextDestroyed() - 어플리케이션이 종료될 때 호출되는 메서드
-		connPool.closeAll();
 	}
 }
